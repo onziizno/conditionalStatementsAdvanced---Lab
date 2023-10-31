b47b7f1538d7f61046dd0d1d4ceaa3ddd69a48bc
onziizno <radonedev@gmail.com>
2023-10-31 19:13:55 +0200

    task 7

--- workingHours.js
+++ workingHours.js
@@ -2,15 +2,30 @@ function workingHours(input) {
 let time = Number(input[0]);
 let day = input[1];
 
-if (time<=18 || time >=10 ) {
-    if (day === 'Monday') {
-        console.log('open')
+/**
+ ase 1: console.log('Monday');break;
+        case 2: console.log('Tuesday');break;
+        case 3: console.log('Wednesday');break;
+        case 4: console.log('Thursday');break;
+        case 5: console.log('Friday');break;
+        case 6: console.log('Saturday');break;
+        case 7: console.log('Sunday');break;
+        default: console.log('Error');break;
+ */
+
+
+if (time >=10 && time <=18 ) {
+    if (day === 'Monday'|| day === "Tuesday" || day === 'Wednesday' || day ==='Thursday' || day === 'Friday' || day === 'Saturday') {
+        console.log ( 'open');
+    } else {
+        console.log ('closed');
     }
+    
 } else {
-    console.log ('closed')
+    console.log ('closed');
 }
 
 }
 
-workingHours(["7",
-"M0nday"])
+workingHours(["12",
+"Monay"])
