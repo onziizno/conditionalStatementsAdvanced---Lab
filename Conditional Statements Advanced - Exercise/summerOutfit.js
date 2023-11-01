b94a80f6e99fff578a1368432025973964c1abf8
onziizno <radonedev@gmail.com>
2023-11-01 22:56:59 +0200

    Update summerOutfit.js

--- Conditional Statements Advanced - Exercise/summerOutfit.js
+++ Conditional Statements Advanced - Exercise/summerOutfit.js
@@ -8,10 +8,34 @@ function summerOutfit(input) {
         if ( timeOfDay ==='Morning') {
             outfit = 'Sweatshirt'
             shoes = 'Sneakers'
+        }else if (timeOfDay === 'Afternoon' || timeOfDay === 'evening') {
+            outfit = 'Shirt'
+            shoes = 'Moccasins'
         }
-    }console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`)
+    }else if (degrees >18 && degrees <=24) {
+       if (timeOfDay === 'Afternoon' || timeOfDay === 'evening') {
+         outfit = 'Shirt'
+         shoes = 'Moccasins'
+        } else if (timeOfDay === 'Afternoon') {
+        outfit = T-Speech
+        shoes = Sandals
+    }
+    }else if(degrees >25) {
+    if ( timeOfDay ==='Morning') {
+        outfit = T-Speech
+        shoes = Sandals
+    } else if (timeOfDay === 'Afternoon') {
+        outfit = Swim ; 
+        shoes = Barefoot;
+    } else if (timeOfDay === ' Evening') {
+        outfit = 'Shirt'
+        shoes = 'Moccasins'
+    }
+}console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`)
+
+
+
 
 }
-
-summerOutfit(["16",
-"Morning"])
+summerOutfit(["22",
+"Afternoon"])
