9f7d1683a219bcbd46803c1caea51ee763813015
onziizno <radonedev@gmail.com>
2023-11-07 19:06:00 +0200

    Create 8 numbersDivisibleBy9.js

---
+++ For Loop - Lab/8 numbersDivisibleBy9.js
@@ -0,0 +1,21 @@
+function numbersDivisibleBy9(input) {
+    let start = Number(input[0]);
+    let stop = Number(input[1]);
+    let sum = 0;
+
+    for (let i = start; i <= stop; i++) {
+        if (i % 9 == 0) {
+            sum += i
+        }
+    }
+    console.log(`The sum: ${sum}`);
+
+    for( let i = start ; i <=stop ; i ++) {
+        if (i %9 == 0) {
+            console.log(i);
+        }
+
+    }
+}
+
+numbersDivisibleBy9(["100", "200"])
