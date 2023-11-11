41a5b843f855f03a24b04d08d67589ba082311fe
onziizno <radonedev@gmail.com>
2023-11-11 17:27:49 +0200

    Create 3 sumNumbersVariant1.js

---
+++ While Loop - Lab/3 sumNumbersVariant1.js
@@ -0,0 +1,25 @@
+function sumNumbers(input) {
+
+    let index = 0;
+    let target = Number(input[index]);
+    index++;
+    
+    sum = 0;
+
+
+    while (target > sum) {
+        
+        let n = Number(input[index]);
+        index++;
+        sum += n;
+    }
+    console.log(sum);
+}
+sumNumbers(["20",
+"1",
+"2",
+"3",
+"4",
+"5",
+"6"])
+
