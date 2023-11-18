7bf63f941d5afa498021dc788fff69d3860a796f
onziizno <radonedev@gmail.com>
2023-11-18 12:48:27 +0200

    Create 5 traveling.js

---
+++ Nested Loops - Lab/5 traveling.js
@@ -0,0 +1,51 @@
+function traveling(input) {
+
+    let index = 0;
+    let command = input[index];
+    index++;
+
+
+    while (command !== "End") {
+        let destinition = command;
+        let budget = Number(input[index]);
+        index++;
+        let sum = 0;
+        while (sum < budget) {
+            let money = Number(input[index]);
+            index++;
+            sum += money;
+
+        }
+        console.log(`Going to ${destinition}!`);
+        command = input[index]
+        index++;
+
+    }
+
+}
+
+traveling(["France",
+"2000",
+"3300",
+"300",
+"200",
+"400",
+"190",
+"258",
+"360",
+"Portugal",
+"1450",
+"400",
+"400",
+"200",
+"300",
+"300",
+"Egypt",
+"1900",
+"1000",
+"280",
+"300",
+"500",
+"End"])
+
+
