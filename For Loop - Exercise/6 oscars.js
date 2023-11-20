c1d4ad9018d1f841b1c15852cadfe4daca24b79e
onziizno <radonedev@gmail.com>
2023-11-20 17:26:57 +0200

    Update 6 oscars.js

--- For Loop - Exercise/6 oscars.js
+++ For Loop - Exercise/6 oscars.js
@@ -1,51 +1,46 @@
-function oscars(input) {
-    let index = 0;
-    let name = input[index];
-    index++;
-    let points = Number(input[index]);
-    index++;
-    let n = Number(input[index]);
-    index++;
-    let isNominee = false;
+function sum(input) {
+  let index = 0;
+  let name = input[index];
+  index++;
+  let points = Number(input[index]);
+  index++;
+  let n = Number(input[index]);
+  index++;
 
-    for (let i = 0; i < n; n++) {
-        let jName = String(input[index]);
-        index++;
-        let currenPoints = Number(input[index]);
-        index++;
-        currenPoints = jName.length * currenPoints / 2;
-        points += currenPoints;
+    for (let i = 0; i < n; i++) {
+      let Jname = String(input[index]);
+      index++
+      let currentPoints = Number(input[index]);
+      index++;
 
-        if (points > 1250.5) {
-            isNominee = true;
-            break;
-        }
+      currentPoints = (Jname.length * currentPoints) /2;
+      points += currentPoints;
 
 
-    }
-    if (isNominee) {
-        console.log(`Congratulations, ${name} got a nominee for leading role with ${points.toFixed(1)}!`);
-    } else {
-        let diff = 1250.5 - points;
-        
-        console.log(`Sorry, ${name} you need ${diff.toFixed(1)} more!`);
-    }
+      if ( points > 1250.5) {
+        break;
+      }
+
+
+   
+  }
+  if ( points >= 1250.5) {
+    console.log(`Congratulations, ${name} got a nominee for leading role with ${points.toFixed(1)}!`);
+  } else {
+    console.log(`Sorry, ${name} you need ${(1250.5 - points).toFixed(1)} more!`);
+  }
 
 }
-
-oscars(["Zahari Baharov",
-"205",
-"4",
-"Johnny Depp",
-"45",
-"Will Smith",
-"29",
-"Jet Lee",
-"10",
-"Matthew Mcconaughey",
-"39"])
-
-
-
-
-
+sum(["Sandra Bullock",
+"340",
+"5",
+"Robert De Niro",
+"50",
+"Julia Roberts",
+"40.5",
+"Daniel Day-Lewis",
+"39.4",
+"Nicolas Cage",
+"29.9",
+"Stoyanka Mutafova",
+"33"])
