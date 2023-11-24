0bcf7b3a9900a050246ba58a275b084bfbc7cc6f
onziizno <radonedev@gmail.com>
2023-11-24 15:28:18 +0200

    Create inchesToCentimeters.js

---
+++ First Steps In Coding - Lab/inchesToCentimeters.js
@@ -0,0 +1,11 @@
+function inchesToCentimeters(input) {
+    let inch = Number(input[0]);
+   
+    let convert = inch*2.54;
+  
+    console.log(convert);
+   
+    
+}
+
+inchesToCentimeters(["5"])
