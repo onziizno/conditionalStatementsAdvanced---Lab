7eedb469fcbc4d835d8fd28e2411eb88745831d3
onziizno <radonedev@gmail.com>
2023-11-24 16:42:38 +0200

    Create 1 oldBooksVariant.js

---
+++ While Loop - Exercise/1 oldBooksVariant.js
@@ -0,0 +1,33 @@
+function oldBooks(input) {
+   let favoriteBook = input[0];
+   let index = 1;
+   let bookIsFound = false;
+
+   let nextBookName = input[index];
+   while (nextBookName !== "No More Books") {
+    if ( nextBookName === favoriteBook) {
+        bookIsFound = true;
+        break;
+    }
+    index++;
+    nextBookName = input[index];
+
+    if (bookIsFound === false) {
+        console.log(`The book you search is not here!`);
+        console.log((`You checked ${index - 1} books.`));
+    } else {
+        console.log(`You checked ${bookCounter} books and found it.`);
+    }
+   }
+
+}
+
+oldBooks(["The Spot",
+"The Spot",
+"Hunger Games",
+"Harry Potter",
+"Torronto",
+"Spotify",
+"No More Books"])
+
+
