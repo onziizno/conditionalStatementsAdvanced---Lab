8c804349790508d0de0025a7b3343bdb8c0a5ac1
onziizno <radonedev@gmail.com>
2023-11-25 18:36:33 +0200

    Update 8 tennisRanklist.js

--- For Loop - Exercise/8 tennisRanklist.js
+++ For Loop - Exercise/8 tennisRanklist.js
@@ -23,7 +23,7 @@ function tennisRanklist(input) {
 
     }
     let finalPoints = points + countFirstPlace * 2000 + countSecondPlace * 1200 + countThirthPlace * 720;
-    let averagePoint =(finalPoints - points) / countTournaments;
+    let averagePoint = (finalPoints - points) / countTournaments;
     let precentWin = (countFirstPlace / countTournaments) * 100;
     console.log(`Final points: ${finalPoints}`);
     console.log(`Average points: ${Math.floor(averagePoint)}`)
@@ -31,13 +31,13 @@ function tennisRanklist(input) {
 }
 
 tennisRanklist(["7",
-"1200",
-"SF",
-"F",
-"W",
-"F",
-"W",
-"SF",
-"W"])
+    "1200",
+    "SF",
+    "F",
+    "W",
+    "F",
+    "W",
+    "SF",
+    "W"])
 
 
